2c423af088a9a3cbf39b2c5ca87b5cd99239d877
Ahtsham-Saddique <ahtshamsaddique55@gmail.com>
2026-02-27 00:12:22 +0500

    initial commit

---
+++ Desktop/jspractice/index.js
@@ -0,0 +1,42 @@
+let arr =[1,2,4,5,5,6,35,6,4,6];
+
+let elm = arr.find((Curelement,index,a)=>
+{
+    return Curelement>5, index,a;
+});
+console.log(elm);
+
+console.log("Hello");
+
+// filtering on base of condition
+
+let value =6;
+let arr2=[1,2,3,4,5,6,7,8,5,7,5];
+
+let elmm = arr2.filter((Curelement2)=>
+{
+    return Curelement2 !=  value;
+})
+;
+console.log(elmm);
+
+
+
+
+let obj= [
+
+    { name:"Ali",age:13},
+    { name:"Ahmad",age:19},
+    { name:"Ali",age:20},
+    { name:"Ali",age:25},
+    
+];
+
+let eligible= obj.filter((Curelement)=>
+{
+    
+
+     return Curelement.age>18;
+});
+
+console.log(eligible);
