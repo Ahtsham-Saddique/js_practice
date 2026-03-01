50d39ace87678120d60adf0eee15e6c3cf675bda
Ahtsham-Saddique <ahtshamsaddique55@gmail.com>
2026-03-01 17:34:59 +0500

    add MORE BASIC FUNTIONS

---
+++ Desktop/jspractice/math.js
@@ -0,0 +1,11 @@
+const pival = Math.PI;
+console.log(pival);
+
+
+// BASICS FUNCTIONS
+console.log(Math.abs(-908978));
+console.log(Math.floor(-1.5));
+console.log(Math.ceil(1.1));
+console.log(Math.round(1.5));
+console.log(Math.trunc(-3.9));
+
