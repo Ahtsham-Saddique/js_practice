3d5fbe7ac2018f6b702059afd4d64ad89a3e7b82
Ahtsham-Saddique <ahtshamsaddique55@gmail.com>
2026-03-01 16:50:06 +0500

    Adding the pangram checking code

--- Desktop/jspractice/strings.js
+++ Desktop/jspractice/strings.js
@@ -12,3 +12,27 @@ console.log(result);
 
 let ch = text.slice(6);
 console.log(ch);
+
+
+
+let pramchk = (str) => {
+    str = str.toLowerCase(); // convert to lowercase
+
+    for (let i = 'a'.charCodeAt(0); i <= 'z'.charCodeAt(0); i++) {
+        let letter = String.fromCharCode(i);
+
+        if (!str.includes(letter)) {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+let resultt = pramchk("The quick brown fox jumps over the lazy dog");
+
+if (resultt) {
+    console.log("Pangram");
+} else {
+    console.log("Not Pangram");
+}
