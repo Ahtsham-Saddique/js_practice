e695b51eff8ca44fc92df8f535ba5df82d482687
Ahtsham-Saddique <ahtshamsaddique55@gmail.com>
2026-03-02 20:01:03 +0500

    add bom practice files

---
+++ Desktop/jspractice/bom.js
@@ -0,0 +1,4 @@
+console.log(window.innerHeight);
+console.log(window.innerWidth);
+    console.log(window.alert("Welocme"));
+// window.innerWidth;
--- Desktop/jspractice/math.js
+++ Desktop/jspractice/math.js
@@ -9,3 +9,35 @@ console.log(Math.ceil(1.1));
 console.log(Math.round(1.5));
 console.log(Math.trunc(-3.9));
 
+
+
+
+
+// exponentional and lograthmitics functions
+
+
+
+let ex= Math.pow(2,9);
+console.log(ex);
+
+
+
+let lg= Math.log(2);
+console.log(lg);
+
+
+
+let lg2= Math.log2(2);
+console.log(lg2);
+
+let sq = Math .sqrt(45);
+console.log(sq);
+
+console.log(6**7);
+
+// for random Number
+
+console.log((Math.random()*1000).toFixed(2));
+
+
+
