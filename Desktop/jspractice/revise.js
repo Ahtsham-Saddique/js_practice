0ee79db50293c482246cda4dd6f97b4365498d47
Ahtsham-Saddique <ahtshamsaddique55@gmail.com>
2026-03-05 23:09:36 +0500

    added the functions concpets

--- Desktop/jspractice/revise.js
+++ Desktop/jspractice/revise.js
@@ -7,3 +7,24 @@ console.log(age);
 const PI = 3.14;
 // PI=3.1 Not allowed
 console.log(PI);
+
+const user = {
+  name: "Ali",
+  age: 22
+};
+
+user.age = 23; // allowed
+
+// functions
+
+function greet(name) {
+  console.log("Hello " + name);
+}
+
+greet("Ali");
+
+// arrow function
+
+const add = (a,b) => {
+ return a + b;
+}
